87e698ba7d26c074ee7c65059667b2b93d6030dd
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-02-28 20:36:11 +0100

    Add start app function to scene delegate Add Dashboard view controller

--- uLessoniOS/app/SceneDelegate.swift
+++ uLessoniOS/app/SceneDelegate.swift
@@ -13,10 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        startApp(scene: scene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,3 +47,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+extension SceneDelegate {
+    private func startApp(scene: UIScene){
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = DashboardViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
---
+++ uLessoniOS/main/module/Dashboard/DashboardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DashboardViewController.swift
+//  uLessoniOS
+//
+//  Created by Mojisola Adebiyi on 28/02/2021.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
