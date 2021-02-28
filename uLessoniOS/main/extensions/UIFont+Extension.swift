ebd30cf919431fc404b90ef02ca19828e8a1dc96
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-02-28 19:11:30 +0100

    Add UIFont extension for itim font

---
+++ uLessoniOS/main/extensions/UIFont+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont+Extension.swift
+//  uLessoniOS
+//
+//  Created by Mojisola Adebiyi on 28/02/2021.
+//
+
+import UIKit
+
+extension UIFont {
+    static func itimRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Itim-Regular", size: size) ?? UIFont()
+    }
+}
