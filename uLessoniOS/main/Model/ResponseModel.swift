e248b0363b5458c503781a5d8e4d7e84bece5f29
Mojisola Adebiyi <mojisolaadebiyi@gmail.com>
2021-02-28 21:45:47 +0100

    Add Response Model

---
+++ uLessoniOS/main/Model/ResponseModel.swift
@@ -0,0 +1,34 @@
+//
+//  ResponseModel.swift
+//  uLessoniOS
+//
+//  Created by Mojisola Adebiyi on 28/02/2021.
+//
+
+import Foundation
+
+struct Response: Codable {
+    let status, message: String?
+    let subjects: [Subject]?
+}
+
+struct Subject: Codable {
+    let id: Int?
+    let name: String?
+    let icon: String?
+    let chapters: [Chapter]?
+}
+
+struct Chapter: Codable {
+    let id: Int?
+    let name: String?
+    let lessons: [Lesson]?
+}
+
+struct Lesson: Codable {
+    let id: Int?
+    let name: String?
+    let icon: String?
+    let mediaURL: String?
+    let subjectID, chapterID: Int?
+}
